60e2b53faaededaf392e23830de20d6370d1179d
Philipp Acsany <68116180+acsany@users.noreply.github.com>
2023-09-12 12:47:17 +0200

    Add Python file which prints glyphs of a given font

---
+++ python/get-glyphset.py
@@ -0,0 +1,8 @@
+from fontTools.ttLib import TTFont
+
+FONTPATH = "fonts/PlayfairDisplay-Medium.ttf"
+
+font = TTFont(FONTPATH)
+glyphset = font.getGlyphSet()
+for glyph in glyphset:
+    print(glyph)
