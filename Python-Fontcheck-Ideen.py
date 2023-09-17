ecb2b3a769e59ad09347860850456ea01823714f
samgrotesk <144777113+samgrotesk@users.noreply.github.com>
2023-09-17 22:10:05 +0200

    Ideenliste erstellt

---
+++ Python-Fontcheck-Ideen.py
@@ -0,0 +1,27 @@
+PYTHON FONTCHECK
+Grundsätzliche Unterschedung zwischen Anwendung in Produktion und nach Export
+
+Charset Check
+    (...) fehlt, (...) ist zu viel
+    Prüfung über Booleans
+    Abgleich mit Sprachliste
+    
+Vertikale Metriken
+    Mindestens mal Output (Konsitenz über Master prüfen)
+    Gegenrechnen (Max), 
+    Diacritics auf jeden Fall mit einbeziehen
+    
+Spacing Check
+    Dezimalprüfung
+    Threshold: Sticht was raus?
+    
+Kerning Check
+    Liste Ausgeben (+ Paare, - Paare)
+    Überlappungs-Alarm (Boolean auf Pfade?)
+    
+Sind Combs richtig angelegt?
+OT-Feature Report
+OT-Feature Bug-Report ?
+Export Report
+    Formate, Instanzen, Stammdicken (Grauwertanstieg)
+Namenlänge Checker (kommt eh nicht drüber aber könnte interessant zu programmieren sein)
